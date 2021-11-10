0478e6df4df75bcba63dbfea23393fa9583d7aab
hthanh2120 <hthanh2120@gmail.com>
2021-11-10 17:12:45 +0700

    update code

--- Activity4/__init__.py
+++ Activity4/__init__.py
@@ -18,9 +18,21 @@ mycursor = mydb.cursor()
 # SELECT DISTINCT maker FROM store_cms_plusplus.laptop ORDER BY maker ASC;
 #
 # 3. Viết câu query trả ra doanh số laptop bán được và doanh thu của cửa hàng.
-# SELECT SUM(sold) AS 'Doanh So', SUM(sold * price) AS 'Doanh Thu' FROM store_cms_plusplus.laptop;
+# SELECT SUM(sold) AS 'Doanh số', SUM(sold * price) AS 'Doanh thu' FROM store_cms_plusplus.laptop;
 
-# 4.2 Viết tính năng thống kê số lượng loại máy tính của mỗi hãng, sắp xếp theo số lượng giảm dần
+# 4.2 Viết chức năng thống kê số lượng, số tiền bán được của mỗi hãng laptop.
+def getCounterByMaker():
+    str = "select maker, count(maker) 'quantity' from laptop group by maker order by quantity desc;"
+    mycursor.execute(str)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        maker = x[0]
+        quantity = x[1]
+        print(f" Maker: {maker}, Quantity: {quantity}")
+
+getCounterByMaker()
+
+# 4.3 Viết tính năng thống kê số lượng loại máy tính của mỗi hãng, sắp xếp theo số lượng giảm dần
 def getStatisticByMaker():
     str = "select maker, sum(sold), sum(sold*price) 'totalMoney' from laptop group by maker ;"
     mycursor.execute(str)
@@ -32,15 +44,3 @@ def getStatisticByMaker():
         print(f" Maker: {maker}, Sold: {sold}, totalMoney: {totalMoney}")
 
 getStatisticByMaker()
-
-# 4.3 Viết chức năng thống kê số lượng, số tiền bán được của mỗi hãng laptop.
-def getCounterByMaker():
-    str = "select maker, count(maker) 'quantity' from laptop group by maker order by quantity desc;"
-    mycursor.execute(str)
-    myresult = mycursor.fetchall()
-    for x in myresult:
-        maker = x[0]
-        quantity = x[1]
-        print(f" Maker: {maker}, Quantity: {quantity}")
-
-getCounterByMaker()
